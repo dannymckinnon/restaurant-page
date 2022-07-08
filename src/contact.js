0e44c44bf54358ea7662c3c4346ec3196a99e9a9
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-07-07 19:54:33 -0700

    Add event listeners to buttons for tabbed navigation

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,5 @@
 function contactPage() {
-
+  alert('Contact page');
 }
 
 
--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ import './style.css';
 
 homePage();
 
+const homeBtn = document.getElementById('home');
+const menuBtn = document.getElementById('menu');
+const contactBtn = document.getElementById('contact');
+const content = document.getElementById('content');
+
+
+homeBtn.addEventListener('click', () => {
+  content.innerHTML = '';
+  homePage();
+});
+
+
+menuBtn.addEventListener('click', () => {
+  content.innerHTML = '';
+  menuPage();
+});
+
+
+contactBtn.addEventListener('click', () => {
+  content.innerHTML = '';
+  contactPage();
+});
